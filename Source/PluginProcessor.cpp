a95cb071d3dac250be6d50a7e42662ca841d977c
2093ab <gil2093ab@gmail.com>
2021-12-24 14:01:14 +0900

    Added KubjwutzRiley Filtering

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -25,17 +25,17 @@ SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
     using namespace Params;
     const auto& params = GetParams();
     
-    auto floatHeloper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
+    auto floatHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
     {
         param = dynamic_cast<juce::AudioParameterFloat*>(apvts.getParameter(params.at(paramName)));
         jassert(param != nullptr);
     };
 
-    floatHeloper(compressor.attack, Names::Attack_Low_Band);
-    floatHeloper(compressor.release, Names::Release_Low_Band);
-    floatHeloper(compressor.threshold, Names::Threshold_Low_Band);
-    floatHeloper(compressor.attack, Names::Attack_Low_Band);
-    floatHeloper(compressor.attack, Names::Attack_Low_Band);
+    floatHelper(compressor.attack, Names::Attack_Low_Band);
+    floatHelper(compressor.release, Names::Release_Low_Band);
+    floatHelper(compressor.threshold, Names::Threshold_Low_Band);
+    floatHelper(compressor.attack, Names::Attack_Low_Band);
+    floatHelper(compressor.attack, Names::Attack_Low_Band);
     
     auto choiceHelper = [&apvts = this->apvts, &params](auto& param, const auto& paramName)
     {
@@ -50,6 +50,10 @@ SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
         jassert(param != nullptr);
     };
     boolHelper(compressor.bypassed, Names::Bypassed_Low_Band);
+    
+    floatHelper(lowCrossover, Names::Low_Mid_Crossover_Freq);
+    LP.setType(juce::dsp::LinkwitzRileyFilterType::lowpass);
+    HP.setType(juce::dsp::LinkwitzRileyFilterType::highpass);
 }
 
 SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor()
@@ -129,6 +133,14 @@ void SimpleMBCompAudioProcessor::prepareToPlay (double sampleRate, int samplesPe
     spec.sampleRate = sampleRate;
     
     compressor.prepare(spec);
+
+    LP.prepare(spec);
+    HP.prepare(spec);
+    
+    for(auto& buffer : filterBuffers)
+    {
+        buffer.setSize(spec.numChannels,samplesPerBlock);
+    }
 }
 
 void SimpleMBCompAudioProcessor::releaseResources()
@@ -179,8 +191,41 @@ void SimpleMBCompAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
         buffer.clear (i, 0, buffer.getNumSamples());
     
   
-    compressor.updateCompressorSettings();
-    compressor.process(buffer);
+    //compressor.updateCompressorSettings();
+    //compressor.process(buffer);
+    for( auto& fb: filterBuffers )
+    {
+        fb = buffer;
+    }
+    
+    auto cutoff = lowCrossover->get();
+    LP.setCutoffFrequency(cutoff);
+    HP.setCutoffFrequency(cutoff);
+    
+    auto fb0Block = juce::dsp::AudioBlock<float> (filterBuffers[0]);
+    auto fb1Block = juce::dsp::AudioBlock<float>(filterBuffers[1]);
+    
+    auto fb0Ctx = juce::dsp::ProcessContextReplacing<float>(fb0Block);
+    auto fb1Ctx = juce::dsp::ProcessContextReplacing<float>(fb1Block);
+    
+    LP.process(fb0Ctx);
+    HP.process(fb1Ctx);
+    
+    auto numSamples = buffer.getNumSamples();
+    auto numChannels = buffer.getNumChannels();
+    
+    buffer.clear();
+
+    auto addFilterBand = [nc = numChannels, ns = numSamples](auto& inputBuffer, const auto& source)
+    {
+        for(auto i=0;i<nc;++i)
+        {
+            inputBuffer.addFrom(i, 0, source, i, 0 , ns);
+        }
+    };
+    
+    addFilterBand(buffer, filterBuffers[0]);
+    addFilterBand(buffer, filterBuffers[1]);
 }
 
 //==============================================================================
@@ -237,6 +282,8 @@ juce::AudioProcessorValueTreeState::ParameterLayout SimpleMBCompAudioProcessor::
     layout.add(std::make_unique<AudioParameterChoice>(params.at(Names::Ratio_Low_Band),params.at(Names::Ratio_Low_Band), sa, 3));
     
     layout.add(std::make_unique<AudioParameterBool>(params.at(Names::Bypassed_Low_Band),params.at(Names::Bypassed_Low_Band), false));
+    
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Low_Mid_Crossover_Freq),params.at(Names::Low_Mid_Crossover_Freq), NormalisableRange<float>(20,20000,1,1),500));
     
     return layout;
 }
